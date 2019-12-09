be4eedee0d485a293cc7ea73b092034f0b350384
Kylee Webb <kawebb@sdsu.edu>
2019-12-08 21:33:07 -0800

    Revert "Merge branch 'master' into UI-branch"
    This reverts commit 666df45f1434aa363db4e6244c5b97aec3d7346f, reversing
    changes made to 6adee6cb2ead8f9ba08739fa7fa1ff6343f9db91.

--- scripts/pricegenerator.js
+++ scripts/pricegenerator.js
@@ -1,25 +1,40 @@
 
 
-
-
-//starts here
- export default  class pricegenerator
+class reservation
 {
-    
+   constructor (_type,_firstname,_lastname,_datereserved, _Arrivaldate,_enddate,roomnumber ){
+      this.billr = 0.0;
+      this.choice = _type;
+      this.firstname = _firstname;
+      this.Lastname = _lastname;
+      this.roomnumber= roomnumber;
+      this.datereserverd = _datereserved;
+      this.Arrivaldate = _Arrivaldate;
+      this.enddate = _enddate;
+   }
+   billrate(){
+     return this.billr;
+   }
+     type() {
+       return this.choice;
+   }
+   roomnumber(){
+     return roomnumber;
+   }
+}
+class hotelrooms {
+  constructor() {
 
-    //takes in the booking information object 
+  }
+}
+
+class pricegenerator
+{
    constructor ( _reservation, _baserate,_expectedAvgOccupancy)
    {
-       //type field var
-       this.type = _reservation.type();
-       //base rate to charge for rooms per night
-       var baserate;
-       //initalizing the baserate for the price gen
+        var baserate;
        baserate = _baserate;
-       
        console.log(  _reservation.type());
-       
-       //changes the rate discount if any based on the type of reservation
             switch (_reservation.type())
        {
            case "Prepaid":
@@ -40,20 +55,22 @@
                break;
        }
       }
-      //allows managers to change the base price 
       setbaserate(_brate){
         return this.baserate = _brate;
       }
-     fetchPrice(){
-         return this.baserate;
-     }
    }
-//ends here
+
+let rese = new reservation("Incentive","martin","Phillip","10/24/2019","11/12/2019","1/1/2020");
+
+let gen = new pricegenerator(rese, 150, 0.5);
+//let roomreseveravtioin = new (rese);
 
 
+gen.setbaserate(220);
 
-
-
-
-
-
+console.log(rese.billrate());
+console.log(rese.firstname);
+console.log(rese.Lastname);
+console.log(rese.datereserverd);
+console.log(rese.Arrivaldate);
+console.log(gen.baserate);
