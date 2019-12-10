614af111b07efefd01bfe83921bc30c9c5a0d4b4
messi <vivekmessikumar@gmail.com>
2019-12-09 20:23:35 -0800

    add hash and modify

--- scripts/modifier.js
+++ scripts/modifier.js
@@ -31,3 +31,17 @@ var isWindow = function isWindow( obj ) {
 		}
 		doc.head.appendChild( script ).parentNode.removeChild( script );
 	}
+
+function isArrayLike( obj ) {
+
+	
+	var length = !!obj && "length" in obj && obj.length,
+		type = toType( obj );
+
+	if ( isFunction( obj ) || isWindow( obj ) ) {
+		return false;
+	}
+
+	return type === "array" || length === 0 ||
+		typeof length === "number" && length > 0 && ( length - 1 ) in obj;
+}
