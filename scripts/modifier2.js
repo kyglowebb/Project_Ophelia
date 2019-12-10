b35da5bcc2f3d29bf4ccfee0e9bf1b3b2a539366
messi <vivekmessikumar@gmail.com>
2019-12-10 00:29:18 -0800

    added modifier for passing data

---
+++ scripts/modifier2.js
@@ -0,0 +1,30 @@
+
+var arr = [];
+
+var document = window.document;
+
+var getProto = Object.getPrototypeOf;
+
+var slice = arr.slice;
+
+var concat = arr.concat;
+
+var push = arr.push;
+
+var indexOf = arr.indexOf;
+
+var class2type = {};
+
+var toString = class2type.toString;
+
+var hasOwn = class2type.hasOwnProperty;
+
+var fnToString = hasOwn.toString;
+
+var ObjectFunctionString = fnToString.call( Object );
+
+var support = {};
+
+var isFunction = function isFunction( obj ) {
+      return typeof obj === "function" && typeof obj.nodeType !== "number";
+  };
