97324dd823a80b2ac71b398ed95bff5755d337f5
messi <vivekmessikumar@gmail.com>
2019-12-10 00:30:35 -0800

    added reject accept logic for data passed

--- scripts/modifier2.js
+++ scripts/modifier2.js
@@ -28,3 +28,29 @@ var support = {};
 var isFunction = function isFunction( obj ) {
       return typeof obj === "function" && typeof obj.nodeType !== "number";
   };
+
+function adoptValue( value, resolve, reject, noValue ) {
+	var method;
+
+	try {
+
+		if ( value && isFunction( ( method = value.promise ) ) ) {
+			method.call( value ).done( resolve ).fail( reject );
+
+		} else if ( value && isFunction( ( method = value.then ) ) ) {
+			method.call( value, resolve, reject );
+
+		} else {
+
+			
+			resolve.apply( undefined, [ value ].slice( noValue ) );
+		}
+
+	
+	} catch ( value ) {
+
+		
+		reject.apply( undefined, [ value ] );
+	}
+}
+
